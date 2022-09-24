a6e24a200e3dc05ff0d62df9160f21db8277d896
xfnw <xfnw@riseup.net>
2022-09-24 19:48:33 -0400

    add tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use tokio;
+use tokio::net::TcpListener;
+
+fn get_listen() -> String {
+    let addr = env::args().nth(1).unwrap_or_else(|| "[::]:6923".to_string());
+    return addr;
+}
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind(get_listen()).await.unwrap();
 }
