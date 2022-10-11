69fc7ba46cad9fd8e83f43ece83e35d178e026d4
xfnw <xfnw@riseup.net>
2022-10-11 19:55:32 -0400

    reformat

--- src/pages/enter.rs
+++ src/pages/enter.rs
@@ -1,13 +1,12 @@
+use format_bytes::format_bytes;
 use std::error::Error;
 use tokio;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::io::{AsyncWriteExt, AsyncReadExt};
-use format_bytes::format_bytes;
 
-pub async fn switch(
-    mut socket: TcpStream,
-    mut user: String,
-) -> Result<(), Box<dyn Error>> {
-    socket.write_all(&format_bytes!(b"hewwo {}",user.as_bytes()) as &[u8]).await?;
+pub async fn switch(mut socket: TcpStream, mut user: String) -> Result<(), Box<dyn Error>> {
+    socket
+        .write_all(&format_bytes!(b"hewwo {}", user.as_bytes()) as &[u8])
+        .await?;
     Ok(())
 }
